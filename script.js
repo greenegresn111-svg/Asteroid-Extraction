ec6cae01d3abc96b3b4b24dab20b136c5b83779d
TARHAV <greenegresn111@gmail.com>
2026-07-29 13:32:49 -0400

    Game balancing + win state
    Fully fixed dyson progress bar
    Balanced game again
    Added win state through buildDysonSwarm()

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let resource_count = 0;
-let drill_upgrade_cost = 100;
-let auto_drill_cost = 200;
+let drill_upgrade_cost = 50;
+let auto_drill_cost = 150;
 let drill_level = 0;
 let auto_drill_count = 0;
 let technology_age = 0;
 let technology_upgrade_cost = 500;
-const dyson_goal = 100000;
+const dyson_goal = 10000;
 
 document.getElementById("drill_upgrade_cost").textContent = drill_upgrade_cost;
 document.getElementById("auto_drill_cost").textContent = auto_drill_cost;
@@ -30,6 +30,19 @@ function updateDysonBar() {
 
     document.getElementById("dyson_fill").style.width = percent + "%";
     document.getElementById("dyson_text").textContent =  `Dyson Swarm Completion ${Math.floor(percent)}%`;
+
+    if (resource_count >= dyson_goal) {
+        document.getElementById("dyson_text").textContent = "BUILD DYSON SWARM";
+    }
+}
+
+function buildDysonSwarm() {
+    if (resource_count >= dyson_goal) {
+        resource_count -= dyson_goal;
+        document.getElementById("resource_count").textContent = resource_count;
+        alert("You have built the Dyson Swarm!");
+        updateDysonBar();
+    }
 }
 
 function extractResource(event) {
@@ -104,21 +117,21 @@ setInterval(() => {
         } else {
             amountGained = auto_drill_count;
         }
+        
+        const asteroid = document.querySelector(".asteroid");
+        /* checks if asteroid is set then spawns floating text NOT WORKING?!?!??!?!?!??!?!?!??!?!? */ 
+        if (asteroid) {
+            const rect = asteroid.getBoundingClientRect();
+            let x = rect.left + (Math.random() * rect.width);
+            let y = rect.top + (Math.random() * rect.height);
+            spawnFloatingText(amountGained, x, y);
+        }
     }
 
     resource_count += amountGained;
     document.getElementById("resource_count").textContent = resource_count;
 
     updateDysonBar();
-
-    const asteroid = document.querySelector(".asteroid");
-    /* checks if asteroid is set then spawns floating text NOT WORKING?!?!??!?!?!??!?!?!??!?!? */ 
-    if (asteroid) {
-        const rect = asteroid.getBoundingClientRect();
-        let x = rect.left + (Math.random() * rect.width);
-        let y = rect.top + (Math.random() * rect.height);
-        spawnFloatingText(amountGained, x, y);
-    }
 }, 1000);
 
 setInterval(() => {
@@ -130,8 +143,8 @@ function reset() {
     auto_drill_count = 0;
     drill_level = 0;
     technology_age = 0;
-    drill_upgrade_cost = 100;
-    auto_drill_cost = 200;
+    drill_upgrade_cost = 50;
+    auto_drill_cost = 150;
     technology_upgrade_cost = 500;
 
     document.getElementById("resource_count").textContent = resource_count;
