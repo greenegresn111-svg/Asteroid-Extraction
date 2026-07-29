f601dcd2488588bb8f4b779f7ae0843792cf0b9c
TARHAV <greenegresn111@gmail.com>
2026-07-29 12:15:46 -0400

    Started implementing changes
    Added shopfront for tech. adv.
    Started adding floating text

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let drill_upgrade_cost = 100;
 let auto_drill_cost = 200;
 let drill_level = 0;
 let auto_drill_count = 0;
+let technology_age = 0;
+let technology_upgrade_cost = 300;
 
 document.getElementById("drill_upgrade_cost").textContent = drill_upgrade_cost;
 document.getElementById("auto_drill_cost").textContent = auto_drill_cost;
+document.getElementById("technology_upgrade_cost").textContent = technology_upgrade_cost;
 
 function extractResource() {
-    resource_count += 1 + drill_level;
+    if (technology_age >= 1) {
+        resource_count += Math.floor(technology_age * 1) + drill_level;
+    } else {
+        resource_count += 1 + drill_level;
+    }
     document.getElementById("resource_count").textContent = resource_count;
 }
 
@@ -32,6 +39,16 @@ function buyAutoDrill() {
     }
 }
 
+function advanceTechnology() {
+    if (resource_count >= technology_upgrade_cost) {
+        resource_count -= technology_upgrade_cost;
+        technology_age += 1;
+        technology_upgrade_cost = Math.floor(technology_upgrade_cost * 2);
+        document.getElementById("resource_count").textContent = resource_count;
+        document.getElementById("technology_upgrade_cost").textContent = technology_upgrade_cost;
+    }
+}
+
 setInterval(() => {
     resource_count += auto_drill_count;
     document.getElementById("resource_count").textContent = resource_count;
@@ -43,7 +60,10 @@ function reset() {
     drill_level = 0;
     drill_upgrade_cost = 100;
     auto_drill_cost = 200;
+    technology_age = 0;
+    technology_upgrade_cost = 500;
     document.getElementById("resource_count").textContent = resource_count;
     document.getElementById("drill_upgrade_cost").textContent = drill_upgrade_cost;
     document.getElementById("auto_drill_cost").textContent = auto_drill_cost;
+    document.getElementById("technology_upgrade_cost").textContent = technology_upgrade_cost;
 }
